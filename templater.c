65ae14481f21dc90ea76b48b1b91b0ed56b524fb
aisaacroth <aisaacroth@gmail.com>
2014-11-22 23:30:46 -0500

    Cleaned up some more. Need to add a header.:

--- templater.c
+++ templater.c
@@ -3,51 +3,38 @@
 #include <string.h>
 
 static void printUsage();
-int isCSClass(char *classNumber);
+void prepareClassInfo(const char **classInfo, char **argv);
+char *prepareFileName(const char **classInfo);
+int isCSClass(const char *classNumber);
 void writeTechnicalTemplate(FILE *texFile, const char **classInfo);
 void setTechnicalPackages(FILE *texFile);
 void writeStandardTemplate(FILE *texFile, const char **classInfo);
+void printGeneratedStatement(const char **classInfo);
 
 int main(int argc, char **argv) {
     if (argc != 5) {
         printUsage();
     }
 
-    char *className = argv[1];
-    char *classNumber = argv[2];
-    char *seriesType = argv[3];
-    char *seriesNumber = argv[4];
-
     const char *classInfo[4];
-    classInfo[0] = className;
-    classInfo[1] = classNumber;
-    classInfo[2] = seriesType;
-    classInfo[3] = seriesNumber;
+    prepareClassInfo(classInfo, argv);
 
-    char *fileName;
-    const char *extension = ".tex";
-    fileName = malloc(strlen(seriesType) + 1 + strlen(seriesNumber) + 
-            strlen(extension));
-    strcpy(fileName, seriesType);
-    strcat(fileName, "-");
-    strcat(fileName, seriesNumber);
-    strcat(fileName, extension);
+    char *fileName = prepareFileName(classInfo);
 
     FILE *texFile = fopen(fileName, "w");
     free(fileName); 
-    if (texFile == NULL) { fprintf(stderr, "Error opening file\n");
+    if (texFile == NULL) { 
+        fprintf(stderr, "Error opening file\n");
         exit(1);
     }
 
-    if (isCSClass(classNumber)) {
+    if (isCSClass(classInfo[1])) {
         writeTechnicalTemplate(texFile, classInfo);
     } else {
         writeStandardTemplate(texFile, classInfo);
     }
 
-
-    printf("Generated template for %s, %s %s\n", className, seriesType, 
-            seriesNumber);
+    printGeneratedStatement(classInfo);
     fclose(texFile);
     return 0;
 }
@@ -58,7 +45,29 @@ static void printUsage() {
     exit(1);
 }
 
-int isCSClass(char *classNumber) {
+void prepareClassInfo(const char **classInfo, char **argv) {
+    char *className = argv[1];
+    char *classNumber = argv[2];
+    char *seriesType = argv[3];
+    char *seriesNumber = argv[4];
+    classInfo[0] = className;
+    classInfo[1] = classNumber;
+    classInfo[2] = seriesType;
+    classInfo[3] = seriesNumber;
+}
+
+char *prepareFileName(const char **classInfo) {
+    const char *extension = ".tex";
+    char *fileName = malloc(strlen(classInfo[2]) + 1 + strlen(classInfo[3]) 
+            + strlen(extension));
+    strcpy(fileName, classInfo[2]);
+    strcat(fileName, "-");
+    strcat(fileName, classInfo[3]);
+    strcat(fileName, extension);
+    return fileName;
+}
+
+int isCSClass(const char *classNumber) {
     return (strstr(classNumber, "COMS") || strstr(classNumber, "CSEE"));
 }
 
@@ -68,6 +77,7 @@ void writeTechnicalTemplate(FILE *texFile, const char **classInfo) {
     fprintf(texFile, "\\title{%s \\\\ %s \\\\ %s %s}\n", classInfo[0], 
             classInfo[1], classInfo[2], classInfo[3]);
     fprintf(texFile, "\\author{}\n");
+    fprintf(texFile, "\\date{}\n");
     fprintf(texFile, "\\maketitle\n");
     fprintf(texFile, "\n");
     fprintf(texFile, "\\end{document}\n");
@@ -93,10 +103,18 @@ void setTechnicalPackages(FILE *texFile) {
 void writeStandardTemplate(FILE *texFile, const char **classInfo) {
     fprintf(texFile, "\\documentclass[]{article}\n");
     fprintf(texFile, "\\usepackage{indentfirst}\n");
+    fprintf(texFile, "\\begin{document}\n");
+    fprintf(texFile, "\n");
     fprintf(texFile, "\\title{%s \\\\ %s \\\\ %s %s}\n", classInfo[0], 
             classInfo[1], classInfo[2], classInfo[3]);
     fprintf(texFile, "\\author{}\n");
+    fprintf(texFile, "\\date{}\n");
     fprintf(texFile, "\\maketitle\n");
     fprintf(texFile, "\n");
     fprintf(texFile, "\\end{document}\n");
+}
+
+void printGeneratedStatement(const char **classInfo) {
+    printf("Generated template for %s, %s %s\n", classInfo[0], classInfo[2], 
+            classInfo[3]);
 }
