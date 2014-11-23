25547191367ff4d9cb844eac20b12cb82f55347d
aisaacroth <aisaacroth@gmail.com>
2014-11-22 19:39:36 -0500

    Basic templater done

--- templater.c
+++ templater.c
@@ -1,16 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 static void printUsage();
 
 int main(int argc, char **argv) {
-    if (argc != 4) {
+    if (argc != 5) {
         printUsage();
     }
+
+    char *className = argv[1];
+    char *classNumber = argv[2];
+    char *seriesType = argv[3];
+    char *seriesNumber = argv[4];
+
+    char *fileName;
+    const char *extension = ".tex";
+    fileName = malloc(strlen(seriesType) + 1 + strlen(seriesNumber) + strlen(extension));
+    strcpy(fileName, seriesType);
+    strcat(fileName, "-");
+    strcat(fileName, seriesNumber);
+    strcat(fileName, extension);
+
+    printf("%s\n", fileName);
+    FILE *texFile = fopen(fileName, "w");
+    free(fileName);
+
+    if (texFile == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        exit(1);
+    }
+
+    if (strstr(classNumber, "COMS") || strstr(classNumber, "CSEE")) {
+        fprintf(texFile, "\\documentclass[]{article}\n");
+        fprintf(texFile, "\\usepackage[all]{xy}\n");
+        fprintf(texFile, "\\usepackage{amsmath}\n");
+        fprintf(texFile, "\\usepackage{amssymb}\n");
+        fprintf(texFile, "\\usepackage{amsthm}\n");
+        fprintf(texFile, "\\usepackage{enumitem}\n");
+        fprintf(texFile, "\\usepackage{indentfirst}\n");
+        fprintf(texFile, "\\usepackage{listings}\n");
+        fprintf(texFile, "\\usepackage{multirow}\n");
+        fprintf(texFile, "\\usepackage{tikz}\n");
+        fprintf(texFile, "\\usepackage{tikz-qtree}\n");
+        fprintf(texFile, "\\usepackage{tipa}\n");
+        fprintf(texFile, "\\begin{document}\n");
+        fprintf(texFile, "\n");
+        fprintf(texFile, "\\newtheorem{thm}{Theorem}\n");
+        fprintf(texFile, "\\title{%s \\\\ %s \\\\ %s \\#%s}\n", classNumber, className, seriesType, seriesNumber);
+        fprintf(texFile, "\\author{}\n");
+        fprintf(texFile, "\\maketitle\n");
+        fprintf(texFile, "\\end{document}\n");
+    } else {
+        fprintf(texFile, "\\documentclass[]{article}\n");
+        fprintf(texFile, "\\usepackage{indentfirst}\n");
+        fprintf(texFile, "\n");
+        fprintf(texFile, "\\title{%s \\\\ %s \\\\ %s \\#%s}\n", classNumber, className, seriesType, seriesNumber);
+        fprintf(texFile, "\\author{}\n");
+        fprintf(texFile, "\\maketitle\n");
+        fprintf(texFile, "\n");
+        fprintf(texFile, "\\end{document}\n");
+    }
+
+
+    printf("%s, %s, %s, %s\n", className, seriesType, classNumber, seriesNumber);
+    fclose(texFile);
     return 0;
 }
 
 static void printUsage() {
-    fprintf(stderr, "usage: templater <class> <series> <number>\n");
+    fprintf(stderr, "usage: templater <class> <class number> <series>"
+            " <series number>\n");
     exit(1);
 }
